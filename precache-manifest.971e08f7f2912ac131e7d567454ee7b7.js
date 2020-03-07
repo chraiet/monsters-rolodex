68a8561c5ca8f6929d5ef323ca2eb7aad6610c82
Momo <chraiet009@outlook.com>
2020-03-07 12:22:35 -0500

    Updates

---
+++ precache-manifest.971e08f7f2912ac131e7d567454ee7b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19a898e2b4eddd8d999b0f77e8a010a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dc37689fad23e9a5893f",
+    "url": "/monsters-rolodex/static/css/main.879a01d8.chunk.css"
+  },
+  {
+    "revision": "d318123e8373fea62b9b",
+    "url": "/monsters-rolodex/static/js/2.ba1b367a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.ba1b367a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc37689fad23e9a5893f",
+    "url": "/monsters-rolodex/static/js/main.bf47d776.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
